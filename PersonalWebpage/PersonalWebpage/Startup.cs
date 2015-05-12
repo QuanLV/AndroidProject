2fe527944ba61350272e99c7ae3fc621bf3e2a8b
QuanLV <quanlv.mr@hotmail.com>
2015-05-12 08:50:34 +0700

    New
    Add new project

---
+++ PersonalWebpage/PersonalWebpage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalWebpage.Startup))]
+namespace PersonalWebpage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
